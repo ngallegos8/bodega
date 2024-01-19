6fa75087b264564f849831ef58efbba7f8eb7ae2
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-19 09:40:38 -0700

    github solution

--- src/components/CurrentInventoryList.js
+++ src/components/CurrentInventoryList.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import InventoryItemCard from './InventoryItemCard';
 
-function CurrentInventoryList() {
+function CurrentInventoryList({inventory, onAddItemToReorder, onDelete}) {
+
+    const inventoryItems = inventory.map(item => {
+        return <InventoryItemCard key={item.id} 
+                                  item={item} 
+                                  onCardClicked={onAddItemToReorder}
+                                  onDelete={onDelete} />
+    });
 
     return(
         <div id="current-inventory">
             <h2>Current Inventory</h2>
             <div>
-                {/** Put inventory items here */}
+                {inventoryItems}
             </div>
         </div>
     );
--- src/components/ReorderInventoryList.js
+++ src/components/ReorderInventoryList.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import InventoryItemCard from './InventoryItemCard';
 
-function ReorderInventoryList() {
+function ReorderInventoryList({inventory, onRemoveItemFromReorder, onDelete}) {
+    const inventoryList = inventory.map(item => {
+        return <InventoryItemCard key={item.id} 
+                                  item={item} 
+                                  onCardClicked={onRemoveItemFromReorder}
+                                  onDelete={onDelete} />
+    });
+
     return(
         <div id="reorder-container">
             <h2>Reorder</h2>
             <div>
-                {/** Put inventory items here */}
+                {inventoryList}
             </div>
         </div>
     );
